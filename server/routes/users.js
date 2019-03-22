035975935c1262caab5353672dd89eb40445d7bf
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-03-22 18:15:28 +0300

    add getCurrentUser controller and auth route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const AuthCtrl = require('../controllers/auth');
 
 const UsersCtrl = require('../controllers/users');
 
 router.get('', UsersCtrl.getUsers);
+router.get('/me', AuthCtrl.onlyAuthUser, UsersCtrl.getCurrentUser);
 
 router.post('/register', UsersCtrl.register);
 router.post('/login', UsersCtrl.login);
