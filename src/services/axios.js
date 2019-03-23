c06d8a91e9937fb1902f147e823ac7a5043b15ba
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-03-23 17:28:19 +0300

    add axios interceptors to fix login,logout bug without refresh page

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,9 +2,23 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   headers: {
-    Authorization: `Bearer ${localStorage.getItem('meetuper-jwt') || ''}`,
     timeout: 3000
   }
 });
+
+axiosInstance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('meetuper-jwt') || '';
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
