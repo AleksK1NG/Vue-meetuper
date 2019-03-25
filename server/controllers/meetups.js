226f79139df195a225018fb612d7f04ce19b6a70
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-03-25 17:16:54 +0300

    add Server Meetups Controlles joinMeetup, leaveMeetup and Meetups routes

--- server/controllers/meetups.js
+++ server/controllers/meetups.js
@@ -1,5 +1,9 @@
 const Meetup = require('../models/meetups');
+const User = require('../models/users');
 
+/*
+ * GET Meetups
+ * */
 exports.getMeetups = function(req, res) {
   Meetup.find({})
     .populate('category')
@@ -13,6 +17,9 @@ exports.getMeetups = function(req, res) {
     });
 };
 
+/*
+ * GET Meetup By Id
+ * */
 exports.getMeetupById = function(req, res) {
   const { id } = req.params;
 
@@ -32,6 +39,9 @@ exports.getMeetupById = function(req, res) {
     });
 };
 
+/*
+ * GET Secret
+ * */
 exports.getSecret = function(req, res) {
   return res.status(200).json({ secret: 'Secret message' });
 };
@@ -51,4 +61,44 @@ exports.createMeetup = function(req, res) {
 
     return res.status(200).json(createdMeetup);
   });
+};
+
+/*
+ * Join Meetup
+ * */
+exports.joinMeetup = function(req, res) {
+  const user = req.user;
+  const { id } = req.params;
+
+  Meetup.findById(id, (errors, meetup) => {
+    if (errors) return res.status(422).send({ errors });
+
+    meetup.joinedPeople.push(user);
+    meetup.joinedPeopleCount++;
+
+    return Promise.all([
+      meetup.save(),
+      User.updateOne({ _id: user.id }, { $push: { joinedMeetups: meetup } })
+    ])
+      .then((result) => res.status(200).json({ id }))
+      .catch((errors) => res.status(422).send({ errors }));
+  });
+};
+
+/*
+ * Leave Meetup
+ * */
+exports.leaveMeetup = function(req, res) {
+  const user = req.user;
+  const { id } = req.params;
+
+  Promise.all([
+    Meetup.updateOne(
+      { _id: id },
+      { $pull: { joinedPeople: user.id }, $inc: { joinedPeopleCount: -1 } }
+    ),
+    User.updateOne({ _id: user.id }, { $pull: { joinedMeetups: id } })
+  ])
+    .then((result) => res.status(200).json({ id }))
+    .catch((errors) => res.status(422).send({ errors }));
 };
