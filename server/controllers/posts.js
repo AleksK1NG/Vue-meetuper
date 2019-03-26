b32dffee8053c056c03e9ca94cdb60e74f9b0059
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-03-26 15:26:28 +0300

    add Server createPost controller and route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,6 +1,9 @@
 const Thread = require('../models/threads');
 const Post = require('../models/posts');
 
+/*
+ * GET Get Posts
+ * */
 exports.getPosts = function(req, res) {
   const threadId = req.query.threadId;
 
@@ -11,6 +14,28 @@ exports.getPosts = function(req, res) {
         return res.status(422).send({ errors });
       }
 
-      return res.json(posts);
+      return res.status(200).json(posts);
     });
+};
+
+/*
+ * POST Create Post
+ * */
+exports.createPost = function(req, res) {
+  const postData = req.body;
+  const post = new Post(postData);
+  post.user = req.user;
+
+  post.save((errors, createdPost) => {
+    if (errors) {
+      return res.status(422).send({ errors });
+    }
+
+    Thread.update(
+      { _id: createdPost.thread },
+      { $push: { posts: createdPost } },
+      () => {}
+    );
+    return res.status(200).json(createdPost);
+  });
 };
