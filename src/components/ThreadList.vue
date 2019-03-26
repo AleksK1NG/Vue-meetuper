13fd047f37926feb906b30d8cc97b0304f801ae9
Alexander Bryksin <alexander.bryksin@yandex.ru>
2019-03-26 13:48:12 +0300

    add ThreadList component

---
+++ src/components/ThreadList.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="content is-medium">
+    <h3 class="title is-3">Threads</h3>
+    <div v-for="thread in threads" :key="thread._id" class="box">
+      <h4 id="const" class="title is-3">
+        {{ thread.title }}
+      </h4>
+
+      <form class="post-create">
+        <div class="field">
+          <textarea
+            class="textarea textarea-post"
+            placeholder="Write a post"
+            rows="1"
+          ></textarea>
+          <button :disabled="true" class="button is-primary m-t-sm">
+            Send
+          </button>
+        </div>
+      </form>
+      <!-- Posts START -->
+      <article
+        v-for="post in thread.posts"
+        :key="post._id"
+        class="media post-item"
+      >
+        <figure class="media-left is-rounded user-image">
+          <p class="image is-32x32">
+            <img class="is-rounded" :src="post.user.avatar" />
+          </p>
+        </figure>
+        <div class="media-content">
+          <div class="content is-medium">
+            <div class="post-content">
+              <strong class="author">{{ post.username }}</strong>
+              {{ ' ' }}
+              <small class="post-time">{{
+                post.updatedAt | formatDate('LLL')
+              }}</small>
+              <br />
+              <p class="post-content-message">{{ post.text }}</p>
+            </div>
+          </div>
+        </div>
+      </article>
+      <!-- Posts END -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ThreadList',
+  props: {
+    threads: {
+      type: Array,
+      required: true
+    },
+    canMakePost: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.content {
+  figure {
+    margin-bottom: 0;
+  }
+}
+.media-content {
+  background-color: #f1f1f1;
+  padding: 3px 20px;
+  border-radius: 10px;
+  margin-right: 40px;
+  width: 100px;
+}
+.media-left.user-image {
+  margin: 0;
+  margin-right: 15px;
+}
+.post-item {
+}
+.media + .media {
+  border: none;
+  margin-top: 0;
+}
+.post-content {
+  margin: 0;
+  &-message {
+    font-size: 16px;
+  }
+  .author {
+    font-size: 18px;
+  }
+  .post-time {
+    font-size: 16px;
+  }
+}
+</style>
--- src/pages/PageMeetupDetail.vue
+++ src/pages/PageMeetupDetail.vue
@@ -118,58 +118,7 @@
               />
             </div>
             <!-- Thread List START -->
-            <div class="content is-medium">
-              <h3 class="title is-3">Threads</h3>
-              <div
-                v-for="thread in orderedThreads"
-                :key="thread._id"
-                class="box"
-              >
-                <!-- Thread title -->
-                <h4 id="const" class="title is-3">
-                  {{ thread.title }}
-                </h4>
-                <!-- Create new post, handle later -->
-                <form class="post-create">
-                  <div class="field">
-                    <textarea
-                      class="textarea textarea-post"
-                      placeholder="Write a post"
-                      rows="1"
-                    ></textarea>
-                    <button :disabled="true" class="button is-primary m-t-sm">
-                      Send
-                    </button>
-                  </div>
-                </form>
-                <!-- Posts START -->
-                <article
-                  v-for="post in thread.posts"
-                  :key="post._id"
-                  class="media post-item"
-                >
-                  <figure class="media-left is-rounded user-image">
-                    <p class="image is-32x32">
-                      <img class="is-rounded" :src="post.user.avatar" />
-                    </p>
-                  </figure>
-                  <div class="media-content">
-                    <div class="content is-medium">
-                      <div class="post-content">
-                        <strong class="author">{{ post.username }}</strong>
-                        {{ ' ' }}
-                        <small class="post-time">{{
-                          post.updatedAt | formatDate('LLL')
-                        }}</small>
-                        <br />
-                        <p class="post-content-message">{{ post.text }}</p>
-                      </div>
-                    </div>
-                  </div>
-                </article>
-                <!-- Posts END -->
-              </div>
-            </div>
+            <ThreadList :threads="orderedThreads" :canMakePost="canMakePost" />
             <!-- Thread List END -->
           </div>
         </div>
@@ -181,10 +130,12 @@
 <script>
 import { mapGetters } from 'vuex';
 import ThreadCreateModal from '../components/ThreadCreateModal';
+import ThreadList from '../components/ThreadList';
 export default {
   name: 'PageMeetupDetail',
   components: {
-    ThreadCreateModal
+    ThreadCreateModal,
+    ThreadList
   },
   computed: {
     ...mapGetters([
@@ -208,6 +159,9 @@ export default {
     },
     canJoin() {
       return !this.isMeetupOwner && this.isAuthenticated && !this.isMember;
+    },
+    canMakePost() {
+      return this.isAuthenticated && (this.isMember || this.isMeetupOwner);
     }
   },
   created() {
@@ -360,48 +314,4 @@ li {
 .footer {
   background-color: white;
 }
-// Post Create Input START
-.textarea-post {
-  padding-bottom: 30px;
-}
-.post-create {
-  margin-bottom: 15px;
-}
-// Post Create END
-// Thread List START
-.content {
-  figure {
-    margin-bottom: 0;
-  }
-}
-.media-content-threads {
-  background-color: #f1f1f1;
-  padding: 3px 20px;
-  border-radius: 10px;
-  margin-right: 40px;
-  width: 100px;
-}
-.media-left.user-image {
-  margin: 0;
-  margin-right: 15px;
-}
-.post-item {
-}
-.media + .media {
-  border: none;
-  margin-top: 0;
-}
-.post-content {
-  margin: 0;
-  &-message {
-    font-size: 16px;
-  }
-  .author {
-    font-size: 18px;
-  }
-  .post-time {
-    font-size: 16px;
-  }
-}
-// Thread List END
 </style>
